c3d5c7b556b1ed79fb4ddd17bbb8f0ba0437386e
ArunGarimella04 <arun.garimella04@gmail.com>
2024-06-28 23:22:03 +0530

    Initial commit

--- proj_code/proj_code.ino
+++ proj_code/proj_code.ino
@@ -52,7 +52,7 @@ bool recordTask = false;
 bool chatgptTask = false;
 
 WiFiClientSecure client;
-ChatGPT<WiFiClientSecure> chat_gpt(&client, "v1", "open-ai key");   // Please fill in your OpenAI key
+//ChatGPT<WiFiClientSecure> chat_gpt(&client, "v1", "open-ai key");   // Please fill in your OpenAI key
 
 
 // Please change to your network
